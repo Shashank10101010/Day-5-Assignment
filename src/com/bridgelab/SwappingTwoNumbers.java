ec4f30431f142b99632c25b55db177656aa2595e
Shashank10101010 <shashankpraveen23@gmail.com>
2022-06-17 15:44:45 +0530

    Swapping of two numbers

---
+++ src/com/bridgelab/SwappingTwoNumbers.java
@@ -0,0 +1,14 @@
+package com.bridgelab;
+
+public class SwappingTwoNumbers {
+        static void Swap( int m, int n) {
+            int temp = m;
+            m = n;
+            n = temp;
+        }
+        public static void main (String[] args){
+            int m = 9, n = 5;
+            Swap(m, n);
+            System.out.println("The value of m is " + m + " and the value of n is " + n);
+        }
+}
