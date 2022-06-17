07ff34dded38ed293377d5cdc105c4e18e6a0b58
Shashank10101010 <shashankpraveen23@gmail.com>
2022-06-17 16:31:42 +0530

    Checking Alphabet and Vowel

--- src/com/bridgelab/SwappingTwoNumbers.java
+++ src/com/bridgelab/SwappingTwoNumbers.java
@@ -1,15 +1,17 @@
 package com.bridgelab;
 
 public class SwappingTwoNumbers {
-        static void Swap( int m, int n) {
-            int temp = m;
-            m = n;
-            n = temp;
-        }
-        public static void main (String[] args){
-            int m = 9, n = 5;
-            Swap(m, n);
-            System.out.println("The value of m is " + m + " and the value of n is " + n);
-        }
+    static void Swap(int m, int n) {
+        int temp = m;
+        m = n;
+        n = temp;
+    }
+
+    public static void main(String[] args) {
+        int m = 9, n = 5;
+        Swap(m, n);
+        System.out.println("The value of m is " + m + " and the value of n is " + n);
+    }
 }
+
 
